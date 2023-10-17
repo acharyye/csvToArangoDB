878c84900c30bd06364d86ca4d4b5161a442e1bf
Akmyrat Charyyev <67785051+acharyye@users.noreply.github.com>
2023-10-17 16:04:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from arango import ArangoClient
 import csv
 
 # ArangoDB configuration
-DB_HOST = "http://big-foxx.skitsanos.com:9999"
-DB_DATABASE = "zeento"
-DB_USERNAME = "zeento"
-DB_PASSWORD = "Dn7HSbhDjx7g5NYQ"
+DB_HOST = "YOURHOST"
+DB_DATABASE = "DATABASENAME"
+DB_USERNAME = "USERNAME"
+DB_PASSWORD = "PASSWORD"
 
 # Initialize ArangoDB client and database connection
 client = ArangoClient(hosts=[DB_HOST])
 db = client.db(DB_DATABASE, username=DB_USERNAME, password=DB_PASSWORD)
 
 # Specify the path to your CSV file
-csv_file = '/Users/akm/FTP/Projects/csvToArangoDB/CSVs to import/pooling-Table 1.csv'
+csv_file = 'YOUR DATA.CSV'
 
 # Specify the collection where you want to insert the data
-collection_name = 'raw_pooling'
+collection_name = 'COLLECTION NAME'
 collection = db.collection(collection_name)
 
 # Open the CSV file and insert its contents into the collection
